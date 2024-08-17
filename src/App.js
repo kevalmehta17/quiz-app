890942ca7ed5a47eec2b913fef738be1c0bc00c1
Keval <kevalmehta885@gmail.com>
2024-08-17 14:42:58 +0530

    Added Server in Scripts inside Package.json for fetching data from fake API and initiated useReducer and useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+
+const initialState = {};
+
+function reducer(state, action) {}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log("Error is:-", err));
+  }, []);
   return (
     <div className="app">
       <Header />
