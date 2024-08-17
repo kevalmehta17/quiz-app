d419609cc5d7e1d2b7132cc4f3d16f224ba77cc6
Keval <kevalmehta885@gmail.com>
2024-08-17 23:40:55 +0530

    Created new Component Folder and placed all component there & also displaying the Option Feature

---
+++ src/components/Options.js
@@ -0,0 +1,14 @@
+function Options({ question }) {
+  return (
+    <div>
+      {question.options.map((option) => (
+        <button className="btn btn-option" key={option}>
+          {" "}
+          {option}{" "}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
