053fd502a0398c9638816fcad78298e94b249ede
Keval <kevalmehta885@gmail.com>
2024-08-23 00:41:12 +0530

    Converting Seconds in Minutes and Seconds

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondRemaining }) {
+  const mins = Math.floor(secondRemaining / 60);
+  const seconds = secondRemaining % 60;
   useEffect(() => {
     const intervalId = setInterval(() => {
       dispatch({ type: "tick" });
@@ -8,7 +10,13 @@ function Timer({ dispatch, secondRemaining }) {
     return () => clearInterval(intervalId);
   }, []);
 
-  return <div className="timer"> {secondRemaining} </div>;
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins} : {seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
 }
 
 export default Timer;
