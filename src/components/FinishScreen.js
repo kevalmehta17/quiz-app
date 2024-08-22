56a82741212472fbb6a9f032f17200626538deb5
Keval <kevalmehta885@gmail.com>
2024-08-22 20:11:48 +0530

    Added the highScore

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,11 +1,20 @@
-function FinishScreen({ points, maxPossiblePoints }) {
+function FinishScreen({ points, maxPossiblePoints, highscore }) {
   const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage <= 100) emoji = "🥈";
+  if (percentage >= 50 && percentage <= 80) emoji = "🥉";
+  if (percentage > 0 && percentage <= 50) emoji = "🎖️";
+  if (percentage === 0) emoji = "🤦‍♂️🤦";
+
   return (
     <div>
       <p className="result">
-        You scored <strong>{points} </strong>out of {maxPossiblePoints} (
-        {Math.ceil(percentage)}%)
+        <span>{emoji}</span> You scored <strong>{points} </strong>out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
+      <p className="highscore">(Highscore:{highscore} points) </p>
     </div>
   );
 }
